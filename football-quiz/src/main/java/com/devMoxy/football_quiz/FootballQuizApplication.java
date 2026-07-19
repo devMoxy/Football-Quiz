0deff1d9ea680dc22eafacdc060017a7e610c731
devMoxy <adelowoenoch1@gmail.com>
2026-07-19 17:44:05 +0100

    initial spring boot setup

---
+++ football-quiz/src/main/java/com/devMoxy/football_quiz/FootballQuizApplication.java
@@ -0,0 +1,13 @@
+package com.devMoxy.football_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballQuizApplication.class, args);
+	}
+
+}
---
+++ football-quiz/src/test/java/com/devMoxy/football_quiz/FootballQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devMoxy.football_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
