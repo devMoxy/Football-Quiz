4e28ec9cdf98ee5dc311ca3bb575ea51f942a5a3
devMoxy <adelowoenoch1@gmail.com>
2026-07-21 17:51:37 +0100

    Category dto

---
+++ football-quiz/src/main/java/com/devMoxy/football_quiz/dto/CategoryDTO.java
@@ -0,0 +1,16 @@
+package com.devMoxy.football_quiz.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+    private Long id;
+
+    private String name;
+}
