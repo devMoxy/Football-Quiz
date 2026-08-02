72253d0fba91e07176d3850441add11b5e6ce8b3
devMoxy <adelowoenoch1@gmail.com>
2026-08-02 15:11:18 +0100

    vite config

--- football-quiz/src/main/java/com/devMoxy/football_quiz/config/CorsConfig.java
+++ football-quiz/src/main/java/com/devMoxy/football_quiz/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         List<String> allowedOrigins = new ArrayList<>();
-        allowedOrigins.add("http://localhost:3000");
+        allowedOrigins.add("http://localhost:5173");
         if (frontendUrl != null && !frontendUrl.isBlank()) {
             allowedOrigins.add(frontendUrl);
         }
